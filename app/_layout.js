947aaa5ad4c1eb03d1b79edbcc051ba6e158626a
Sukrit Saha <sukritsaha27@gmail.com>
2023-10-19 18:08:21 +0530

    finishing setup and welcome screen

---
+++ app/_layout.js
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+
+export default function HomeLayout() {
+  // const [fontsLoaded] = useFonts({
+  //   DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+  //   DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+  //   DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  // });
+
+  // if (!fontsLoaded) {
+  //   return null;
+  // }
+
+  return <Stack />;
+}
---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useRouter } from "expo-router";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  TextInput,
+  Image,
+} from "react-native";
+
+import styles from "./welcome.style";
+import { SIZES, icons } from "../../../constants";
+
+const jobTypes = ["Full-time", "Part-time", "Internship", "Freelance"];
+
+const Welcome = () => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time");
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Sukrit</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
