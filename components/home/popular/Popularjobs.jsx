3ba7b59285f0309a5e8bfa584e67815471eb2a7f
Sukrit Saha <sukritsaha27@gmail.com>
2023-10-20 17:48:57 +0530

    completing popular jobs

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,64 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './popularjobs.style'
+import styles from "./popularjobs.style";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import { COLORS, SIZES } from "../../../constants";
+import { useFetch } from "../../../hooks/useFetch";
 
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
+  const [selectedJob, setSelectedJob] = useState(null);
 
-export default Popularjobs
+  const { data, error, isLoading } = useFetch("search", {
+    query: "Next.js Developer",
+    num_pages: "1",
+  });
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something Went Wrong</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
+              />
+            )}
+            keyExtractor={(item) => item.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
---
+++ hooks/useFetch.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+// import { RAPID_API_KEY } from "@env";
+import axios from "axios";
+
+// const rapidAPIKEY = RAPID_API_KEY;
+
+export const useFetch = (endPoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endPoint}`,
+    params: {
+      ...query,
+    },
+    headers: {
+      "X-RapidAPI-Key": "1e91dd8c5fmsh985878a4f98e8b8p15d130jsnd58d376950f0",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+    } catch (error) {
+      setError(error);
+      alert("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
