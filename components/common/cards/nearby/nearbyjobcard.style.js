cf33f7771de842ef55499bb2a586bde5c0e88d41
Sukrit Saha <sukritsaha27@gmail.com>
2023-10-20 18:00:55 +0530

    completing nearby jobs

--- components/common/cards/nearby/nearbyjobcard.style.js
+++ components/common/cards/nearby/nearbyjobcard.style.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  logImage: {
+  logoImage: {
     width: "70%",
     height: "70%",
   },
@@ -32,12 +32,11 @@ const styles = StyleSheet.create({
   },
   jobName: {
     fontSize: SIZES.medium,
-    fontFamily: "DMBold",
+    fontWeight: "600",
     color: COLORS.primary,
   },
   jobType: {
     fontSize: SIZES.small + 2,
-    fontFamily: "DMRegular",
     color: COLORS.gray,
     marginTop: 3,
     textTransform: "capitalize",
