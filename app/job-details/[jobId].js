e2581cb0c804a1bd8b7a8328a62835411631425d
Sukrit Saha <sukritsaha27@gmail.com>
2023-10-21 16:37:47 +0530

    completing job description page

--- app/job-details/[jobId].js
+++ app/job-details/[jobId].js
@@ -109,6 +109,13 @@ const JobDetails = () => {
             </View>
           )}
         </ScrollView>
+
+        <JobFooter
+          url={
+            data[0]?.job_google_link ??
+            "https://careers.google.com/jobs/results"
+          }
+        />
       </>
     </SafeAreaView>
   );
